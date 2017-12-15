128ae40ad55724762fb7b17527484b27b5437372
Arash Hatami <hatamiarash7@gmail.com>
2017-12-15 18:09:18 +0330

    First Release

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(998, 230);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 365);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(998, 243);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(827, 282);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 35);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Sentence Splitter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(678, 282);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 35);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Word Tokenize";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(384, 282);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(143, 35);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "POS";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(533, 282);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(139, 35);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "StopWords";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(28, 282);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(132, 35);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "load";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1022, 620);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,100 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NHazm;
 
 namespace Persian_NLP
 {
     public partial class Form1 : Form
     {
+        string[] lines;
+        Normalizer normalizer;
+        SentenceTokenizer senTokenizer;
+        WordTokenizer wordTokenizer;
+        POSTagger tagger;
         public Form1()
         {
             InitializeComponent();
+            lines = System.IO.File.ReadAllLines("stopwords.txt");
+            normalizer = new Normalizer(true, false, false);
+            senTokenizer = new SentenceTokenizer();
+            wordTokenizer = new WordTokenizer(true);
+            tagger = new POSTagger();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+            string input = textBox1.Text;
+            string[] output = senTokenizer.Tokenize(input).ToArray();
+            for (int i = 0; i < output.Length; i++)
+            {
+                textBox2.Text = textBox2.Text + output[i] + Environment.NewLine;
+            }
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+            string input = textBox1.Text;
+            string[] output = senTokenizer.Tokenize(input).ToArray();
+            for (int i = 0; i < output.Length; i++)
+            {
+                string[] final = wordTokenizer.Tokenize(output[i]).ToArray();
+                for (int j = 0; j < final.Length; j++)
+                {
+                    textBox2.Text = textBox2.Text + final[j] + Environment.NewLine;
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+            string input = textBox1.Text;
+            string[] output = senTokenizer.Tokenize(input).ToArray();
+            for (int i = 0; i < output.Length; i++)
+            {
+                string[] final = wordTokenizer.Tokenize(output[i]).ToArray();
+                for (int j = 0; j < final.Length; j++)
+                {
+                    if (!lines.Contains(final[j]))
+                        textBox2.Text = textBox2.Text + final[j] + Environment.NewLine;
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+            string input = textBox1.Text;
+            string[] output = senTokenizer.Tokenize(input).ToArray();
+            string[] removed;
+            List<string> temp = new List<string>();
+            for (int i = 0; i < output.Length; i++)
+            {
+                string[] final = wordTokenizer.Tokenize(output[i]).ToArray();
+                for (int j = 0; j < final.Length; j++)
+                {
+                    if (!lines.Contains(final[j]))
+                        temp.Add(final[j]);
+
+                }
+            }
+            removed = temp.ToArray();
+            POSTagger posTagger = new POSTagger();
+            //posTagger.BatchTag(removed).t;
         }
     }
 }
